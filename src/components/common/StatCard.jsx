adef907658f3b16ac59d834d7c5f1ffe7c4a2f4b
Zreon <56314835+zreonx@users.noreply.github.com>
2024-09-12 12:56:45 +0800

    added stat card

--- src/components/common/StatCard.jsx
+++ src/components/common/StatCard.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { Icon } from "lucide-react";
 
-function StatCard() {
-  return <div>stat</div>;
+function StatCard({ name, icon: Icon, value, color }) {
+  return (
+    <motion.div
+      className='bg-gray-800 bg-opacity-50 backdrop-blur-md overflow-hidden shadow-lg rounded-xl border border-gray-700'
+      whileHover={{
+        y: -5,
+        boxShadow: "0 25px -12px rgba(0,0,0,0.5)",
+      }}
+    >
+      <div className='px-4 sm:p-6'>
+        <span className='flex items-center text-sm font-medium text-gray-400'>
+          <Icon size={20} className='mr-2 ' style={{ color }} /> {name}
+        </span>
+        <p className='mt-1 text-3xl font-semibold text-gray-100'> {value}</p>
+      </div>
+    </motion.div>
+  );
 }
 
 export default StatCard;
--- src/pages/OverViewPage.jsx
+++ src/pages/OverViewPage.jsx
@@ -1,5 +1,25 @@
 import Header from "../components/common/Header";
-import { motion } from "framer-motion";
+import { color, motion } from "framer-motion";
+import StatCard from "../components/common/StatCard";
+import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
+import React from "react";
+
+const STATS = [
+  { name: "Total Sales", icon: Zap, value: "$12,345", color: "#6366f" },
+  { name: "New Users", icon: Users, value: "1,234", color: "#8B5CF6" },
+  {
+    name: "Total Products",
+    icon: ShoppingBag,
+    value: "567",
+    color: "#EC4899",
+  },
+  {
+    name: "Conversion Rate",
+    icon: BarChart2,
+    value: "12.5%",
+    color: "#10B981",
+  },
+];
 
 const OverViewPage = () => {
   return (
@@ -7,7 +27,23 @@ const OverViewPage = () => {
       <Header title='Overview' />
       <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8 xl:px-12'>
         {/* Stats */}
-        <motion.div className=''></motion.div>
+        <motion.div
+          className='grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          {STATS.map((item) => (
+            <React.Fragment key={item.name}>
+              <StatCard
+                name={item.name}
+                icon={item.icon}
+                value={item.value}
+                color={item.color}
+              />
+            </React.Fragment>
+          ))}
+        </motion.div>
       </main>
     </div>
   );
